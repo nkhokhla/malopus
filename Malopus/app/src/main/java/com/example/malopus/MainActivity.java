43f51134ebf0ea88c8c1b6b2d46dbe20fd2c3136
Nazar Khokhla <nazikhello@gmail.com>
2020-02-21 21:19:37 +0200

    Bottom nav new design

--- Malopus/app/src/main/java/com/example/malopus/MainActivity.java
+++ Malopus/app/src/main/java/com/example/malopus/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.malopus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.home:
+
+                            case R.id.library:
+
+                            case R.id.news:
+
+                            case R.id.guides:
+                        }
+                        return true;
+                    }
+                });
+
     }
 }
